755dfb21f173b883a92cc5455b38aa10f1844885
Ganeshp007 <potdart40.gp@gmail.com>
2022-04-29 00:01:43 +0530

    Added: UC7_EmpwageClass

---
+++ Employee_Classes/CalEmpwage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Classes
+{
+    internal class CalEmpwage
+    {
+
+
+        public static int TotalEmpwage()
+        {
+
+            int Is_Fulltime = 1;
+            int Is_Parttime = 2;
+            int Emp_Rate_Hr = 20;
+            int Emp_Hr = 0;
+            int Emp_wage = 0;
+            Random obj = new Random();
+            int empcheck = obj.Next(0, 3); //it will generate 0 and 1 only bcoz its limit is n-1 ie 2-1=1
+
+            if (empcheck == Is_Fulltime)
+            {
+                Console.WriteLine("Employee is Fulltime");
+                Emp_Hr = 8;
+            }
+            else if (empcheck == Is_Parttime)
+            {
+                Console.WriteLine("Employee is Parttime");
+                Emp_Hr = 4;
+            }
+            else
+            {   Emp_Hr = 0;
+                Console.WriteLine("Employee is Absent");
+            }
+
+            Emp_wage = Emp_Rate_Hr * Emp_Hr;
+            Console.WriteLine("\nDailywage of current Empolyee is :- " + Emp_wage);
+
+            return Emp_wage;
+            
+
+        }
+    }
+}
---
+++ Employee_Classes/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Employee_Classes;
+
+Console.WriteLine("----- Welcome to Employee Management System -----\n");
+Console.WriteLine("Press ENter to Continue...\n");
+Console.ReadKey();
+Console.WriteLine("\nWage of current Empolyee is :- " + CalEmpwage.TotalEmpwage());
+   
